bd0e5fd4a5aae8177032a06af37753eebc12e09e
Pedro <pefernandez1408@gmail.com>
2020-06-06 00:31:43 -0430

    Usando parciales con HBS

--- server.js
+++ server.js
@@ -1,27 +1,38 @@
 const express = require('express')
 const app = express();
 
+const hbs = require('hbs');
+
 app.use( express.static( __dirname + '/public' ) ); //Archivos de dominio publico
+
+// Express HBS engine
+hbs.registerPartials( __dirname + '/views/parcials' );
+app.set('view engine', 'hbs');
+
  
-// app.get('/', (req, res) => {
-// //   res.send('Hello World')
-// let salida = {
-//              nombre: 'Pedro',
-//              edad: 24,
-//              url: req.url
-//          };
+app.get('/', (req, res) => {
+
+      res.render('home', {
+         nombre: 'Pedro',
+         anio: new Date().getFullYear()
+      });
+
+});
+
+app.get('/about', (req, res) => {
+
+       res.render('about', {
+          anio: new Date().getFullYear()
+       });
+});
+
+// app.get('/data', (req, res) => {
+//   res.send('Hello World')
+
 
 //       res.send(salida);
 
 // });
-
-app.get('/data', (req, res) => {
-  res.send('Hello World')
-
-
-      res.send(salida);
-
-});
  
 app.listen(3000, () => {
    console.log('Escuchando peticiones en el puerto 3000');
