dfcf67a97749704b7cf61c2e6e7614e0ce1ae0c9
Pedro <pefernandez1408@gmail.com>
2020-06-06 20:11:18 -0430

    Usando helpers

--- server.js
+++ server.js
@@ -3,27 +3,28 @@ const app = express();
 
 const hbs = require('hbs');
 
+require('./hbs/helpers');
+
 app.use( express.static( __dirname + '/public' ) ); //Archivos de dominio publico
 
 // Express HBS engine
 hbs.registerPartials( __dirname + '/views/parcials' );
 app.set('view engine', 'hbs');
 
+
+
  
 app.get('/', (req, res) => {
 
       res.render('home', {
-         nombre: 'Pedro',
-         anio: new Date().getFullYear()
+         nombre: 'Pedro'
       });
 
 });
 
 app.get('/about', (req, res) => {
 
-       res.render('about', {
-          anio: new Date().getFullYear()
-       });
+       res.render('about');
 });
 
 // app.get('/data', (req, res) => {
